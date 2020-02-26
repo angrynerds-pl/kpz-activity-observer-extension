f9830040532e19c06a02c4cbaa1a77f004869323
Artur Boryś <artur.borys13@gmail.com>
2020-02-26 22:40:13 +0100

    Add password confirmation validator

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  ValidationErrors,
-  ValidatorFn
-} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { IInputs } from "@app/shared/interfaces";
 import { fadeIn, fadeOut } from "@app/shared/animations";
@@ -61,39 +55,50 @@ export class RegisterComponent implements OnInit {
   }
 
   initForm() {
-    this.registerForm = this.formBuilder.group({
-      name: ["", Validators.compose([Validators.required])],
-      surname: ["", Validators.compose([Validators.required])],
-      email: ["", Validators.compose([Validators.required, Validators.email])],
-      password: [
-        "",
-        Validators.compose([
-          Validators.required,
-          Validators.minLength(8),
-          Validators.maxLength(30)
-        ])
-      ],
-      confirmPassword: [
-        "",
-        Validators.compose([
-          Validators.required,
-          Validators.minLength(8),
-          Validators.maxLength(30)
-        ])
-      ]
-    });
+    this.registerForm = this.formBuilder.group(
+      {
+        name: ["", Validators.compose([Validators.required])],
+        surname: ["", Validators.compose([Validators.required])],
+        email: [
+          "",
+          Validators.compose([Validators.required, Validators.email])
+        ],
+        password: [
+          "",
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(30)
+          ])
+        ],
+        confirmPassword: [
+          "",
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(30)
+          ])
+        ]
+      },
+      { validator: this.checkPasswords }
+    );
   }
 
-  checkPasswords: ValidatorFn = (
-    control: FormGroup
-  ): ValidationErrors | null => {
+  checkPasswords(control: FormGroup) {
     const password = control.get("password");
     const confirmPassword = control.get("confirmPassword");
 
-    const passwordsEqual = password === confirmPassword;
+    const passwordsEqual = password.value === confirmPassword.value;
 
-    return passwordsEqual ? null : { mismatchingPasswords: true };
-  };
+    if (passwordsEqual) {
+      confirmPassword.setErrors(null);
+    } else {
+      confirmPassword.setErrors({
+        passwordMismatch: true,
+        ...confirmPassword.errors
+      });
+    }
+  }
 
   submit() {
     this.isSubmitted = true;
