44a8468ea88e05d4fe2c60ce2fcdfe7b1aa29499
Artur Boryś <artur.borys13@gmail.com>
2020-03-17 17:02:51 +0100

    Download and display user statistics

--- src/app/modules/statistics/pages/statistics/statistics.component.ts
+++ src/app/modules/statistics/pages/statistics/statistics.component.ts
@@ -1,45 +1,41 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AppApiService } from "@app/core/api/app-api.service";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
-  selector: 'app-statistics',
-  templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.scss']
+  selector: "app-statistics",
+  templateUrl: "./statistics.component.html",
+  styleUrls: ["./statistics.component.scss"]
 })
 export class StatisticsComponent implements OnInit {
-  single: any[] = [
-    {
-      "name": "Sadistic",
-      "value": 8940000
-    },
-    {
-      "name": "Kwejk",
-      "value": 5000000
-    },
-    {
-      "name": "YouTube",
-      "value": 7200000
-    },
-    {
-      "name": "Facebook",
-      "value": 6200000
-    }
-  ];
+  single: any[] = [];
+  loaded = false;
 
-  constructor() {
-  }
+  constructor(private appApiService: AppApiService) {}
 
   ngOnInit() {
+    this.appApiService.getStatistics().subscribe(response => {
+      const holder = [];
+      response.data.forEach(site => {
+        holder.push({
+          name: site.url,
+          value: site.timestamps.length
+        });
+      });
+      this.single = holder;
+      this.loaded = true;
+    });
   }
 
   onSelect(data): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+    console.log("Item clicked", JSON.parse(JSON.stringify(data)));
   }
 
   onActivate(data): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)));
+    console.log("Activate", JSON.parse(JSON.stringify(data)));
   }
 
   onDeactivate(data): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+    console.log("Deactivate", JSON.parse(JSON.stringify(data)));
   }
 }
--- src/app/modules/statistics/statistics.module.ts
+++ src/app/modules/statistics/statistics.module.ts
@@ -1,20 +1,18 @@
-import {StatisticsRoutingModule} from "@app/modules/statistics/statistics-routing.module";
-import {SharedModule} from "@app/shared/shared.module";
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
+import { StatisticsRoutingModule } from "@app/modules/statistics/statistics-routing.module";
+import { SharedModule } from "@app/shared/shared.module";
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
-import * as Pages from './pages';
-
+import * as Pages from "./pages";
 
 @NgModule({
-  declarations: [
-    Pages.StatisticsComponent
-  ],
+  declarations: [Pages.StatisticsComponent],
   imports: [
     CommonModule,
     SharedModule,
-    StatisticsRoutingModule
+    StatisticsRoutingModule,
+    MatProgressSpinnerModule
   ]
 })
-export class StatisticsModule {
-}
+export class StatisticsModule {}
